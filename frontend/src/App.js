3e7d0e355530728c4ab84ae8d8b846623edaf4df
Breno Nogueira <bcnogueira@id.uff.br>
2018-09-08 19:00:20 -0300

    feat: Show posts.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import * as Api from './utils/api';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faCaretUp, faCaretDown } from '@fortawesome/free-solid-svg-icons';
+import { faCalendarAlt, faEdit, faTrashAlt, faComments } from '@fortawesome/free-regular-svg-icons';
+import { formatDate } from './utils/helper';
 
 class App extends Component {
   state = {
@@ -21,6 +25,45 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        {this.state.posts.map(post => (
+          <div key={post.id} className="w-30 mx-3 p-3 post-box">
+            <div className="post-box-category w-30 mx-auto">{post.category}</div>
+            <div className="row mt-2">
+              <div className="col-1 d-flex flex-column justify-content-around align-items-center">
+                <FontAwesomeIcon icon={faCaretUp} className="fa-3x" />
+                {post.voteScore}
+                <FontAwesomeIcon icon={faCaretDown} className="fa-3x" />
+              </div>
+              <div className="col-8 d-flex flex-column justify-content-around align-items-center">
+                <h5>{post.title}</h5>
+                <h6>
+                  <FontAwesomeIcon icon={faUser} className="mr-2 fa-lg" />
+                  {post.author}
+                </h6>
+                <h6>
+                  <FontAwesomeIcon icon={faCalendarAlt} className="mr-2 fa-lg" />
+                  {formatDate(post.timestamp)}
+                </h6>
+              </div>
+              <div className="col-3 d-flex flex-column justify-content-around align-items-center">
+                <div className="align-middle">
+                  <FontAwesomeIcon icon={faComments} className="fa-lg mr-2" />
+                  {post.commentCount}
+                </div>
+                <div className="btn-group-vertical" role="group">
+                  <button type="button" className="btn btn-outline-primary text-left">
+                    <FontAwesomeIcon icon={faEdit} className="mr-2" />
+                    Edit
+                  </button>
+                  <button type="button" className="btn btn-outline-danger text-left">
+                    <FontAwesomeIcon icon={faTrashAlt} className="mr-2" />
+                    Delete
+                  </button>  
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     );
   }
